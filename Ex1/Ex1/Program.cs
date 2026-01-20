c2c1d29b62221bbf3b43715e36c3f8a50d7acb96
Xiro ₍^. .^₎⟆ <100165004+BadlandsXiro@users.noreply.github.com>
2026-01-20 07:25:24 +0200

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+
+        if (a != 0)
+        {
+            double x = -b / a;
+            Console.WriteLine("Solutia este x = " + x);
+        }
+        else
+        {
+            if (b == 0)
+                Console.WriteLine("Infinit de solutii");
+            else
+                Console.WriteLine("Nu exista solutii");
+        }
+    }
+}
