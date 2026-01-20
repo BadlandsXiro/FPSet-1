88ec44fa37672d7e19c6b6d289ee3892ffbbb9c9
Xiro ₍^. .^₎⟆ <100165004+BadlandsXiro@users.noreply.github.com>
2026-01-20 07:34:03 +0200

    Add files via upload

---
+++ Ex14/Ex14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int copie = n, inv = 0;
+        while (n > 0)
+        {
+            inv = inv * 10 + n % 10;
+            n /= 10;
+        }
+
+        Console.WriteLine(copie == inv ? "Palindrom" : "Nu este palindrom");
+    }
+}
---
+++ Ex16/Ex16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int x = a, y = b;
+        while (y != 0)
+        {
+            int r = x % y;
+            x = y;
+            y = r;
+        }
+
+        int cmmdc = x;
+        int cmmmc = a * b / cmmdc;
+
+        Console.WriteLine("CMMDC = " + cmmdc);
+        Console.WriteLine("CMMMC = " + cmmmc);
+    }
+}
