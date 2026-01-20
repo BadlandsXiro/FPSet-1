4e87b1932bb199be95765dcda6e241d72973e513
Xiro ₍^. .^₎⟆ <100165004+BadlandsXiro@users.noreply.github.com>
2026-01-20 07:33:19 +0200

    Add files via upload

---
+++ Ex10/Ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        bool prim = n > 1;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                prim = false;
+
+        Console.WriteLine(prim ? "Numar prim" : "Nu este prim");
+    }
+}
